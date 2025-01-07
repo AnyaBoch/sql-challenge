818fce223b7bad70749adf79fd5bc1d11b38a275
Anya Bocharova <anya.bocharo@gmail.com>
2025-01-06 20:59:41 -0800

    Update table_schema.sql

--- table_schema.sql
+++ table_schema.sql
@@ -29,7 +29,7 @@ CREATE TABLE employees (
 -- creating "dept_emp" table which has two composite keys
 CREATE TABLE dept_emp (
     emp_no INT not null,
-    dept_no VARCHAR(4) not null,
+    dept_no VARCHAR(5) not null,
     PRIMARY KEY (emp_no, dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
